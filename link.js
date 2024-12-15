02b8dcfad53bc85679911c135fcedaa305c5f818
Amina EL Guenuni <88779300+2005101@users.noreply.github.com>
2024-12-16 00:52:41 +0100

    Add files via upload

---
+++ link.js
@@ -0,0 +1,4 @@
+const profileButton = document.getElementById("profile-button");
+profileButton.addEventListener("click", function () {
+    window.location.href = "https://www.linkedin.com/in/amina0289/"; // Redirect to LinkedIn
+});
